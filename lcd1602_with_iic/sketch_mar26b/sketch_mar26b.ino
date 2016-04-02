bde6aed0a2ae3f0d1e0c2bfa5f59065a8b7bfce0
Dzmitry Samsonau <dzmitrysamsonau@gmail.com>
2016-04-02 17:15:17 +0300

    Added time sketches

--- lcd1602_with_iic/sketch_mar26b/sketch_mar26b.ino
+++ lcd1602_with_iic/sketch_mar26b/sketch_mar26b.ino
@@ -15,16 +15,6 @@
 #define D6_pin  6
 #define D7_pin  7
 
-byte heart[8] = {
-  B00000,
-  B01010,
-  B11111,
-  B11111,
-  B01110,
-  B00100,
-  B00000,
-};
-
 LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 
 void setup()
@@ -36,29 +26,24 @@ void setup()
 // Switch on the backlight
 lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
 lcd.setBacklight(HIGH);
- lcd.setCursor(5, 0);
- lcd.print("Hi, dad");  
 
-  lcd.createChar(0, heart);
-  lcd.setCursor(6, 1); 
-  lcd.write(byte(0));
-
-  lcd.createChar(0, heart);
-  lcd.setCursor(8, 1); 
-  lcd.write(byte(0));
-
-  lcd.createChar(0, heart);
-  lcd.setCursor(10, 1); 
-  lcd.write(byte(0));
+  lcd.setCursor(4, 1); 
+  lcd.print(timeString());
 }
 
 void loop()
 {
- // Backlight on/off every 3 seconds
-// lcd.setCursor (0,1);        // go to start of 2nd line
-// lcd.print(n++,DEC);
-// lcd.setBacklight(LOW);      // Backlight off
-// delay(3000);
-// lcd.setBacklight(HIGH);     // Backlight on
-// delay(3000);
+time_t t = now(); 
+
+  lcd.setCursor(4, 1); 
+  lcd.print(timeString());
+  
+}
+
+String timeString() {
+  return printDigits(hour()) + ":" + printDigits(minute()) + ":" + printDigits(second());
+}
+
+String printDigits(int digits){
+    return digits < 10 ?  "0" + String(digits): String(digits);
 }
